b9526ed16d04cf4a6a179e2ec54be0710c22b7a2
Johnathen8 <108311672+Johnathen8@users.noreply.github.com>
2022-07-10 04:32:58 -0400

    Update PlayerVaults.php

--- src/muqsit/playervaults/PlayerVaults.php
+++ src/muqsit/playervaults/PlayerVaults.php
@@ -1,9 +1,6 @@
 <?php
-
 declare(strict_types=1);
-
 namespace muqsit\playervaults;
-
 use muqsit\invmenu\InvMenuHandler;
 use muqsit\playervaults\database\Database;
 use muqsit\playervaults\database\Vault;
@@ -17,28 +14,22 @@ use RuntimeException;
 use function gettype;
 use function is_array;
 use function is_string;
-
 final class PlayerVaults extends PluginBase{
-
 	private Database $database;
 	private PermissionManager $permission_manager;
-
 	protected function onEnable() : void{
 		$this->initVirions();
 		$this->createDatabase();
 		$this->loadConfiguration();
 	}
-
 	protected function onDisable() : void{
 		$this->getDatabase()->close();
 	}
-
 	private function initVirions() : void{
 		if(!InvMenuHandler::isRegistered()){
 			InvMenuHandler::register($this);
 		}
 	}
-
 	private function createDatabase() : void{
 		$this->saveDefaultConfig();
 
@@ -48,7 +39,6 @@ final class PlayerVaults extends PluginBase{
 		}
 		$this->database = new Database($this, $database);
 	}
-
 	private function loadConfiguration() : void{
 		$inventory_name = $this->getConfig()->get("inventory-name");
 		Vault::setNameFormat(is_string($inventory_name) ? $inventory_name : null);
@@ -56,38 +46,32 @@ final class PlayerVaults extends PluginBase{
 		$this->saveResource("permission-grouping.yml");
 		$this->permission_manager = new PermissionManager(new Config($this->getDataFolder() . "permission-grouping.yml", Config::YAML));
 	}
-
 	public function getDatabase() : Database{
 		return $this->database;
 	}
-
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 		if(!($sender instanceof Player)){
-			$sender->sendMessage(TextFormat::RED . "This command can only be executed as a player.");
+			$sender->sendMessage("This command can only be executed as a player.");
 			return true;
 		}
-		
 		if(isset($args[0])){
 			$number = (int) $args[0];
 			if($number > 0){
 				$player = $sender->getName();
-
 				if(isset($args[1]) && strtolower($args[1]) !== strtolower($player)){
 					if($sender->hasPermission("playervaults.others.view")){
 						$player = $args[1];
 					}else{
-						$sender->sendMessage(TextFormat::RED . "You don't have permission to view " . $args[1] . "'s vault #" . $number . ".");
+						$sender->sendMessage("You don't have permission to view " . $args[1] . "'s vault #" . $number . ".");
 						return false;
 					}
 				}else{
 					if(!$this->permission_manager->hasPermission($sender, $number)){
-						$sender->sendMessage(TextFormat::RED . "You don't have permission to use vault #" . $number . ".");
+						$sender->sendMessage("You don't have permission to use vault #" . $number . ".");
 						return false;
 					}
 				}
-
 				$sender->sendMessage(TextFormat::GRAY . "Opening" . ($player === $sender->getName() ? "" : " " . $player . "'s") . " vault #" . $number . "...");
-
 				$this->getDatabase()->loadVault($player, $number, function(Vault $vault) use($sender) : void{
 					if($sender->isConnected()){
 						$vault->send($sender);
@@ -96,8 +80,7 @@ final class PlayerVaults extends PluginBase{
 				return true;
 			}
 		}
-
-		$sender->sendMessage(TextFormat::RED . "Usage: /" . $label . " <number> " . ($sender->hasPermission("playervaults.others.view") ? "[player=YOU]" : ""));
+		$sender->sendMessage("Usage: /" . $label . " <number> " . ($sender->hasPermission("playervaults.others.view") ? "[player=YOU]" : ""));
 		return false;
 	}
 }
